8b32d8c6e760a1d4b1b33c11bc195c7ea0ded62c
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-05-29 11:47:13 +0200

    css 2

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -1,39 +1,50 @@
-class Usuario{
-	constructor (dni,edad,apellidos,nombre) {
-	    this.dni = dni;
-	    this.edad = edad;
-	    this.nombre = nombre;
-	    this.apellidos = apellidos;
-	    }
+class Usuario {
+	constructor(dni, edad, apellidos, nombre) {
+		this.dni = dni;
+		this.edad = edad;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
 }
 
-function GuardarDatos(formulario){
+function GuardarDatos(formulario) {
 
-//se guardan los datos del formulario
-var dni=formulario.dni.value;
-var edad=formulario.edad.value;
-var nombre=formulario.nombre.value;
-var apellidos=formulario.apellidos.value;
+	//se guardan los datos del formulario
+	var dni = formulario.dni.value;
+	var edad = formulario.edad.value;
+	var nombre = formulario.nombre.value;
+	var apellidos = formulario.apellidos.value;
 
-//creamos objeto usuario
-var usuario = new Usuario(dni,edad,apellidos,nombre);
+	//creamos objeto usuario
+	var usuario = new Usuario(dni, edad, apellidos, nombre);
 
-//guardamos objeto en memoria local/   con dni se guarda en memoria
-localStorage.setItem(dni, JSON.stringify(usuario));//JSON.stringify: convierte objeto a cadena JSON
+	//guardamos objeto en memoria local/   con dni se guarda en memoria
+	localStorage.setItem(dni, JSON.stringify(usuario));//JSON.stringify: convierte objeto a cadena JSON
 
+	//localStorage.setItem("persona" + dni, usuario);
 }
 
-function Consultar(formulario2){
-	
-//guardamos el dni a consultar
-var dniConsultar = formulario2.dni.value;
+function Consultar(formulario2) {
 
-//JSON.parse: convierte cadena JSON a objeto
-var obj = JSON.parse(localStorage.getItem(dniConsultar));
+	//guardamos el dni a consultar
+	var dniConsultar = formulario2.dni.value;
 
-//se muestran datos del objeto elegido
-document.write("<br>Nombre: "+obj.nombre)
-document.write("<br>Apellidos: "+obj.apellidos)
-document.write("<br>Edad: "+obj.edad)
-document.write("<br>DNI: "+obj.dni)
+	//JSON.parse: convierte cadena JSON a objeto
+	var obj = JSON.parse(localStorage.getItem(dniConsultar));
+
+	//se muestran datos del objeto elegido
+	document.write("<br>Nombre: " + obj.nombre)
+	document.write("<br>Apellidos: " + obj.apellidos)
+	document.write("<br>Edad: " + obj.edad)
+	document.write("<br>DNI: " + obj.dni)
 }
+
+/*
+function Consultar(formulario2) {
+	var dniConsultar = formulario2.dni.value;
+	var objPersonaString = localStorage.getItem("persona" + dniConsultar);
+	var objPersona = JSON.parse(objPersonaString);
+
+	document.write("nombre"+objPersona.nombre);
+}
+*/
